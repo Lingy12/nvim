f9df64b25905a86e4442cee2b216e395e1be6ea9
Lingy12 <russellgy1229@gmail.com>
2023-06-28 22:53:42 +0800

    init

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,5 @@
+-- default not open
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup()
---
+++ lua/plugins/plugins-setup.lua
@@ -0,0 +1,71 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional
+    }
+  }
+  
+  use("christoomey/vim-tmux-navigator")
+  
+  use "nvim-treesitter/nvim-treesitter" -- syntax highlighting
+  use "p00f/nvim-ts-rainbow"
+  
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig"
+  }
+
+    use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "L3MON4D3/LuaSnip" -- snippet engin
+  use "saadparwaiz1/cmp_luasnip"
+  use "rafamadriz/friendly-snippets"
+  use "hrsh7th/cmp-path" -- file path
+
+  use "numToStr/Comment.nvim" -- gcc
+  use "windwp/nvim-autopairs" -- bracket
+  use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+  use "lewis6991/gitsigns.nvim" -- git hint
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',  -- file explore
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
