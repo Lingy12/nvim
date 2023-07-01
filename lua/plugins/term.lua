df6b747d7a13738eea68e806ef592a641dea6327
Lin Geyu <geyu.lin@u.nus.edu>
2023-07-01 09:49:51 +0800

    change terminal open shortcut

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -12,6 +12,6 @@ end
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-vim.keymap.set('n', '<C-t>', "<cmd>ToggleTerm<CR>")
-vim.keymap.set('n', '<C-s>', "<cmd>TermSelect<CR>")
-vim.keymap.set('n', '<C-a>', "<cmd>ToggleTermToggleAll<CR>")
+vim.keymap.set('n', '<leader>t', "<cmd>ToggleTerm<CR>")
+vim.keymap.set('n', '<leader>s', "<cmd>TermSelect<CR>")
+vim.keymap.set('n', '<leader>a', "<cmd>ToggleTermToggleAll<CR>")
